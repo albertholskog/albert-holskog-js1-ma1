6a8ce85c7e980e520ae73c231442d715b0e9c123
albertholskog <91052944+albertholskog@users.noreply.github.com>
2022-01-27 21:08:10 +0100

    done

--- js/script.js
+++ js/script.js
@@ -1,62 +1,71 @@
 // Question 1
 const cat = {
-    complain: "Meow!",
-    funcCat: function(){
-        return this.complain;
-    }
-}
+  complain: "Meow!",
+  funcCat: function () {
+    return this.complain;
+  },
+};
 console.log(cat.complain);
 
 // Question 2
 
-const heading = document.querySelector("h3")
+const heading = document.querySelector("h3");
 
 heading.innerHTML = "Updated heading";
 
 // Question 3
 
-heading.style.fontSize = "2em"
-
+heading.style.fontSize = "2em";
 
 // Question 4
 
-heading.classList.add("subheading")
+heading.classList.add("subheading");
 
 // Question 5
 
 const paragraphs = document.querySelectorAll("p");
 for (let i = 0; i < paragraphs.length; i++) {
-    paragraphs[i].style.color = "red";
+  paragraphs[i].style.color = "red";
 }
-
 
 // Question 6
 
 const resultsContainer = document.querySelector(".results");
-resultsContainer.innerHTML = "<p>New paragraph</p>"
+resultsContainer.innerHTML = "<p>New paragraph</p>";
 resultsContainer.style.backgroundColor = "yellow";
-
 
 // Question 7
 
 const cats = [
-    {
-        name: "Blob",
-        age: 10
-    },
-    {
-        name: "Harold",
-    },
-    {
-        name: "Blurt",
-        age: 21
-    }
+  {
+    name: "Blob",
+    age: 10,
+  },
+  {
+    name: "Harold",
+  },
+  {
+    name: "Blurt",
+    age: 21,
+  },
 ];
 
-function list (){
-    for(let i = 0; i < cats.length; i++)
-    console.log(cats[i].name);
+function list() {
+  for (let i = 0; i < cats.length; i++) console.log(cats[i].name);
 }
-list()
+list();
 
-// Question 8
+function createCats(cats) {
+  let html = "";
+  for (let i = 0; i < cats.length; i++) {
+    let ageUn = "Age Unknown";
+    if (cats[i].age) {
+      ageUn = cats[i].age;
+    }
+    html += document.querySelector(
+      ".cat-container"
+    ).innerHTML += `<div> <h5>${cats[i].name}</h5><p>${ageUn}</p></div>`;
+  }
+  return html;
+}
+createCats(cats);
